432207512513b5b6895c573e7c5d13155f3e440c
Harshit <harshitchaudhary82004@gmail.com>
2024-09-01 02:08:05 +0530

    added IO class

--- src/app/fileHandling/IO.hpp
+++ src/app/fileHandling/IO.hpp
@@ -1,10 +1,21 @@
-//Inclusion Guards -->  prevent from the multiple inclusion of same header file multiple time.
+// Inclusion Guards -->  prevent from the multiple inclusion of same header file
+// multiple time.
 #ifndef IO_HPP
 #define IO_HPP
 
+#include <fstream>
+#include <string>
 
 
+class IO {
+public:
+  IO(const std::string &file_path);
+  ~IO();
 
+  std::fstream getFileStream();
 
+private:
+  std::fstream file_stream;
+};
 
 #endif
