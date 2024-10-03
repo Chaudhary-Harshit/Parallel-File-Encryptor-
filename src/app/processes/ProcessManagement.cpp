8312c6a0aead9a4e9692ccf8f7026cf49df93a73
Chaudhary-Harshit <harshitchaudhary82004@gmail.com>
2024-10-03 19:54:53 +0530

    ProcessManagement Done

--- src/app/processes/ProcessManagement.cpp
+++ src/app/processes/ProcessManagement.cpp
@@ -1,4 +1,20 @@
 #include <iostream>
 #include <cstring>
 #include "ProcessManagement.hpp"
+#include <sys/wait.h>
 
+ProcessManagement:: ProcessManagement(){}
+
+bool ProcessManagement:: submitToQueue(std::unique_ptr<Task> task){
+    taskQueue.push(std::move(task));
+    return true;
+}
+
+void ProcessManagement:: executeTasks(){
+    while(!taskQueue.empty()){
+        std:: unique_ptr<Task> taskToExecute= std:: move(taskQueue.front());
+        taskQueue.pop();
+        std::cout<<"Executing task: "<<taskToExecute->to_String()<<std::endl;
+        // executeCryption(taskToExecute->to_String());
+    }
+}
